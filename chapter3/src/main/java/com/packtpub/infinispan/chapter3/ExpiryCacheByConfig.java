db2b38c0bf3a00e84fca0d0c61868f771eac8b8b
Alvaro Jose Peralta Ocampo <alvaro@lab4u.cl>
2014-06-12 17:06:09 -0400

    add many changes to the project to work in eclipse

--- chapter3/src/main/java/com/packtpub/infinispan/chapter3/ExpiryCacheByConfig.java
+++ chapter3/src/main/java/com/packtpub/infinispan/chapter3/ExpiryCacheByConfig.java
@@ -1,45 +1,36 @@
 package com.packtpub.infinispan.chapter3;
 
-import java.io.IOException;
-import java.util.Set;
 import java.util.UUID;
+
+import model.Ticket;
 
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
 
-import model.Ticket;
-import utils.IOUtils;
-
-
-
 public class ExpiryCacheByConfig {
 
-	public void start()  {
-		DefaultCacheManager m=null;
+	public void start() {
+		DefaultCacheManager m = null;
 		try {
 			m = new DefaultCacheManager("sample.xml");
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Cache<Integer, Ticket> cache = m.getCache("evictionCache");        
+		Cache<Integer, Ticket> cache = m.getCache("evictionCache");
 		int ticketid = 1;
 
-		while (ticketid < 1500){
+		while (ticketid < 1500) {
 
 			String name = UUID.randomUUID().toString();
 			String show = UUID.randomUUID().toString();
 
-			Ticket ticket = new Ticket(name,show);
+			Ticket ticket = new Ticket(name, show);
 			cache.put(ticketid, ticket);
 			ticketid++;
 		}
-		log("Booked tickets:   "+ticketid);
-		log("Tickets in Cache: "+cache.size());
-
-
-
-
+		log("Booked tickets:   " + ticketid);
+		log("Tickets in Cache: " + cache.size());
 
 	}
 
@@ -47,7 +38,8 @@ public class ExpiryCacheByConfig {
 		new ExpiryCacheByConfig().start();
 
 	}
-	public static void log(String s){
+
+	public static void log(String s) {
 		System.out.println(s);
 	}
 }
